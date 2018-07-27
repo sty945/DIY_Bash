5f7415233203be13cebac8c70f7aec128fb728eb
sty945 <sutaoyu@yeah.net>
2018-07-27 09:47:50 +0800

    将文件夹的文件以用户友好可读方式排序

--- sort_file.sh
+++ sort_file.sh
@@ -6,6 +6,5 @@
 # github: https://github.com/sty945/DIY_Bash
 
 
-
 du -s * | sort -nr | awk -F'\t' '{if(1024 * 1024 * 1024 * 1024 > $1 && $1 >= 1024 * 1024 * 1024) {printf "%.2fT\t\t %-2s\n", $1/(1024 * 1024 * 1024), $2} else if(1024 * 1024 * 1024 > $1 && $1 >= 1024 * 1024) {printf "%.2fG\t\t %-2s\n", $1/(1024 * 1024), $2} else if (1024 * 1024 > $1 && $1 >= 1024) {printf "%.2fM\t\t %-2s\n", $1/1024, $2} else {printf "%sk\t\t %-2s\n", $1, $2}}' 
 
